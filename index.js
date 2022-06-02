adc10867ae8774b994d9d7d9bb6cc7f6e5d47c2c
Yahya <yahyahussein670@gmail.com>
2022-06-02 16:32:09 +0300

    commit tree cc83670ab8074c51ecdb8ddf914ea9b6399515d9

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+const letters = ['Guadalupe', 'Ollie', 'Aki'];
+function writeCards(letters) {
+    let sentence = []
+    for (let i = 0; i < letters.length; i++) {
+    sentence.push(`Thank you, ${letters[i]}, for the wonderful surprise gift!`);
+    }
+    return sentence;
+}
+
+
+let decrement = 100;
+
+function countDown(decrement){
+    while (decrement >= 0) {
+        console.log(decrement); 
+        decrement -=1;
+    }
+}
+countDown(decrement);
